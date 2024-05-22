9a13147e22ddb7ac935ef17699893c88ce541601
dragoje <dragojejanjevic@gmail.com>
2024-05-22 20:04:06 +0200

    Impelemented new tests for add three items to cart.

--- src/test/java/org/code/saucedemo/tests/InventoryTests.java
+++ src/test/java/org/code/saucedemo/tests/InventoryTests.java
@@ -23,6 +23,12 @@ public class InventoryTests {
                                {"sauce-labs-fleece-jacket", "sauce-labs-bolt-t-shirt"}};
     }
 
+    @DataProvider(name = "data-provider-three-items")
+    public Object[][] returnThreeItemsName() {
+        return new Object[][] {{"sauce-labs-onesie", "sauce-labs-fleece-jacket", "sauce-labs-backpack"},
+                               {"sauce-labs-backpack", "sauce-labs-fleece-jacket", "sauce-labs-bolt-t-shirt"}};
+    }
+
     @Test(dataProvider = "data-provider")
     public void addItemToCart(String itemName) {
         System.setProperty("webdriver.chrome.driver", "D:\\Driver\\chromedriver.exe");
@@ -62,6 +68,29 @@ public class InventoryTests {
         System.out.println("Cart number " + inventoryPage.showNumberItemInCart());
 
         Assert.assertEquals(inventoryPage.showNumberItemInCart(), 2, "Number count is not as expected");
+
+        inventoryPage.close();
+
+    }
+
+    @Test(dataProvider = "data-provider-three-items")
+    public void addThreeItemsToCart(String firstItemName, String secondItemName, String thirdItem) {
+        System.setProperty("webdriver.chrome.driver", "D:\\Driver\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        loginPage.setUserName("standard_user");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickOnLogin();
+
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        inventoryPage.addItemToCart(firstItemName);
+        inventoryPage.addItemToCart(secondItemName);
+        inventoryPage.addItemToCart(thirdItem);
+        System.out.println("Cart number " + inventoryPage.showNumberItemInCart());
+
+        Assert.assertEquals(inventoryPage.showNumberItemInCart(), 3, "Number count is not as expected");
 
         inventoryPage.close();
 
